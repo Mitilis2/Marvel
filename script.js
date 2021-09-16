dcd8c00368154e60dd2797249c6168372417c3c0
Mitul Shah <mitulshah91@gmail.com>
2021-09-16 07:20:16 -0400

    finish styling newsletter

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+var learnMoreButton1 = document.getElementById('phase-one-learn-more');
+
+var moviesButton1 = document.getElementById('phase-one-movies')
+
+var showsButton1 = document.getElementById('shows-btn-one')
+
+
+var removeHidden = function(){
+    moviesButton1.classList.toggle('hidden')
+    showsButton1.classList.toggle('hidden')
+    moviesButton1.classList.toggle('clicked')
+    showsButton1.classList.toggle('clicked')
+
+}
+
+
+learnMoreButton1.addEventListener('click', removeHidden)
+
